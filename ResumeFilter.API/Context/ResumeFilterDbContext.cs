86c6951a4bacc0a4e93479539358061ffdd5b696
Prem Kumar M <c203342@ardentmills.com>
2023-09-27 10:39:56 +0530

    Add project files.

---
+++ ResumeFilter.API/Context/ResumeFilterDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ResumeFilter.API.Models;
+
+namespace ResumeFilter.API.Context;
+
+public class ResumeFilterDbContext : DbContext
+{
+    public ResumeFilterDbContext(DbContextOptions<ResumeFilterDbContext> options)
+        : base(options)
+    {
+
+    }
+
+    public DbSet<Employee> Employees { get; set; }
+}
---
+++ ResumeFilter.API/Models/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace ResumeFilter.API.Models;
+
+public class Employee
+{
+	public int Id { get; set; }
+	public string Name { get; set; }
+	public string Role { get; set; }
+}
